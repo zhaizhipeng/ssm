8bf2e99e1b57797e29fc4b2c0b656aa1e966b207
zhaizhipeng <zhaizhipeng@extracme.com>
2019-04-02 19:03:30 +0800

    静态代理

---
+++ ssm-service/src/main/java/com/ysdrzp/ssm/aop/proxy/statics/TransactionManager.java
@@ -0,0 +1,16 @@
+package com.ysdrzp.ssm.aop.proxy.statics;
+
+public class TransactionManager {
+
+    public void begin(){
+        System.out.println("开启事务");
+    }
+
+    public void commit(){
+        System.out.println("事物提交");
+    }
+
+    public void rollBack(){
+        System.out.println("关闭事务");
+    }
+}
