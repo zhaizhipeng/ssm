9e5d78d7d342176ad27d042e28da36b29d93d403
zhaizhipeng <zhaizhipeng@extracme.com>
2019-04-09 18:41:41 +0800

    搭建转账环境

---
+++ ssm-service/src/main/java/com/ysdrzp/ssm/tx/IAccountService.java
@@ -0,0 +1,16 @@
+package com.ysdrzp.ssm.tx;
+
+/**
+ * @author zhipeng zhai
+ * @date 2019/4/9 0009
+ */
+public interface IAccountService {
+
+    /**
+     * 转账
+     * @param inId
+     * @param outId
+     * @param balance
+     */
+    void transfer(long inId, long outId, double balance);
+}
